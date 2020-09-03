1181cf3219c7ef3417a36c35b957d231b7e1949c
Vicg2k21 <70721864+Vicg2k21@users.noreply.github.com>
2020-09-03 14:50:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualsStudio2019_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
